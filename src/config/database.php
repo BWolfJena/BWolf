b88fed9cff78fedf591e8f84c2dc999ed5f4c0fa
Christoph <staudtc@live.de>
2017-11-19 15:03:16 +0100

    Adjust database config for development

--- src/config/database.php
+++ src/config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'mysql'),
             'port' => env('DB_PORT', ''),
-            'database' => env('DB_DATABASE', 'database'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'bwolf'),
+            'username' => env('DB_USERNAME', 'bwolf'),
+            'password' => env('DB_PASSWORD', 'secret'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
