6658472263b0ea1ebfd8fe88dca803823c1933d8
CSNWEB <christoph@kiron.university>
2017-11-26 12:45:50 +0100

    Mirror to public directory

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ sudo docker-compose up -d
 sleep 1
 sudo docker-compose exec --user=laradock workspace composer install
 sudo docker-compose exec --user=laradock workspace php artisan october:up
+sudo docker-compose exec --user=laradock workspace php artisan october:mirror public/
