5fb9ebd5d519e14ce6467f7fa6f347543a4988ce
CSNWEB <christoph@kiron.university>
2017-12-08 01:56:04 +0100

    Implement new course priority selection component

--- src/plugins/bwolfjena/core/components/CourseSelection.php
+++ src/plugins/bwolfjena/core/components/CourseSelection.php
@@ -1,20 +1,18 @@
 <?php namespace BwolfJena\Core\Components;
 
+use Auth;
+use Flash;
 use Cms\Classes\ComponentBase;
-use BWolfJena\Core\Models\Dragndrop;
+use BWolfJena\Core\Models\Course;
+use Bwolfjena\Core\Models\UserCoursePriority;
+use Pheanstalk\Exception;
 
-class Dragndrop extends ComponentBase
+class CourseSelection extends ComponentBase
 {
-    /**
-    * all courses
-    * @var serialized array
-    */
-    public $courses = Course::all();
-
     public function componentDetails()
     {
         return [
-            'name'        => 'Dragndrop Component',
+            'name'        => 'Course selection Component',
             'description' => 'Lists all available courses in non-preffered<li>',
         ];
     }
@@ -26,7 +24,49 @@ class Dragndrop extends ComponentBase
 
     public function courses()
     {
-        return Course::all();
+        $courses = Course::all()->shuffle();
+        if(!Auth::check()) {
+            return  $courses;
+        }
+        $user = Auth::getUser();
+        $order = UserCoursePriority::where('user_id', $user->id)->orderBy('priority', 'DESC')->get();
+        if ($order->isEmpty()) {
+            return $courses;
+        } else {
+            return $order->map(function($coursePriority) use ($courses) {
+                return $courses->first(function($course) use ($coursePriority) {
+                   return $course->id == $coursePriority->course_id;
+                });
+            });
+        }
+    }
+
+    public function  onRun()
+    {
+        $this->addJs('//cdn.jsdelivr.net/npm/sortablejs@1.6.1/Sortable.min.js');
+        $this->addJs('assets/js/course_selection.js');
+    }
+
+    public function onSaveOrder()
+    {
+        if (!Auth::check()) {
+            Flash::error('Sie müssen angemeldet sein.');
+            return;
+        }
+        $order  = post('order');
+        $courseCount = $this->courses()->count();
+        $toStore = [];
+        $user = Auth::getUser();
+        foreach ($order as $index => $id) {
+            $toStore[] = [
+                'user_id' => $user->id,
+                'course_id' => $id,
+                'priority' => $courseCount - $index,
+            ];
+        }
+        UserCoursePriority::where('user_id', $user->id)->delete();
+        UserCoursePriority::insert($toStore);
+        Flash::success('Deine Reihenfolge wurde gespeichert.');
     }
 
 }
