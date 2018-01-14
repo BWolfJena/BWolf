d61ec03646714972325d60e405439e6142891729
CSNWEB <christoph@kiron.university>
2018-01-14 22:34:40 +0100

    Trigger algorithmus in backend and render diagrams

--- DistributionExamples/tranformToSQL.js
+++ DistributionExamples/tranformToSQL.js
@@ -3,6 +3,6 @@ const distribution = require('./normalPreferences.json');
 const keys = Object.keys(distribution);
 keys.forEach((key, userId) => {
   distribution[key].forEach((courseId, priority) => {
-    console.log(`INSERT INTO bwolf.bwolfjena_core_user_course_priorities(course_id,user_id,priority) VALUES (${courseId},${userId+1},${priority+1});`);
+    console.log(`INSERT INTO bwolf.bwolfjena_core_user_course_priorities(course_id,user_id,priority) VALUES (${courseId},${userId+1},${(15-priority)});`);
   });
 });
--- src/plugins/bwolfjena/core/controllers/Distribution.php
+++ src/plugins/bwolfjena/core/controllers/Distribution.php
@@ -23,11 +23,13 @@ class Distribution extends Controller
         BackendMenu::setContext('BwolfJena.Core', 'core', 'distribution');
     }
 
-    public function distribute($id) {
+    public function distribute($id)
+    {
         $client = new \GuzzleHttp\Client();
         $module = Module::findOrFail($id);
-        $courses = $module->courses->map(function($course) {
+        $courses = $module->courses->keyBy('id')->map(function ($course) {
             return [
+                'id' => $course->id,
                 'min' => $course->min_participants,
                 'max' => $course->max_participants,
             ];
@@ -36,21 +38,75 @@ class Distribution extends Controller
             ->orderBy('priority', 'DESC')
             ->get()
             ->groupBy('user_id')
-            ->map(function($grouped){
-               return $grouped->map(function($course){
-                   return $course->course_id;
-               });
-            })->toArray();
-        $response = $client->post('https://bwolfalgorithm.herokuapp.com/', [
+            ->map(function ($grouped) {
+                return $grouped->map(function ($course) {
+                    return $course->course_id;
+                });
+            })
+            ->slice(0, 130)
+            ->toArray();
+        $response = json_decode($client->post('https://bwolfalgorithm.herokuapp.com/', [
             'json' => [
                 'courses' => $courses,
                 'elections' => $elections,
                 'params' => [
-                    'min' => 0,
-                    'weights'=> range(1, count($courses)),
+                    'lowest' => 0,
+                    'weights' => range(count($courses), 1),
                 ],
             ]
-        ]);
-        dd(json_decode($response->getBody()->getContents()));
+        ])->getBody()->getContents());
+        $this->vars['results'] = [];
+        if (!isset($response->error)) {
+            $this->vars['results'][] = [
+                'heading' => 'Lineare Gewichte ohne Minimumsbegrenzung',
+                'histPreferences' => $response->histPreferences,
+                'histCourses' => $response->histCourses,
+                'mean' => $response->mean,
+            ];
+            $min = $response->min;
+            while (!isset($response->error)) {
+                $min++;
+                $response = json_decode($client->post('https://bwolfalgorithm.herokuapp.com/', [
+                    'json' => [
+                        'courses' => $courses,
+                        'elections' => $elections,
+                        'params' => [
+                            'lowest' => $min,
+                            'weights' => range(count($courses), 1),
+                        ],
+                    ]
+                ])->getBody()->getContents());
+                if (!isset($response->error)) {
+                    $this->vars['results'][] = [
+                        'heading' => 'Lineare Gewichte mit Minimum ' . $min,
+                        'histPreferences' => $response->histPreferences,
+                        'histCourses' => $response->histCourses,
+                        'mean' => $response->mean,
+                    ];
+                }
+            }
+            for ($i = 2; $i <= 6; $i=$i+2) {
+                $response = json_decode($client->post('https://bwolfalgorithm.herokuapp.com/', [
+                    'json' => [
+                        'courses' => $courses,
+                        'elections' => $elections,
+                        'params' => [
+                            'lowest' => 0,
+                            'weights' => collect(range( 1, count($courses)))->map(function ($number) use ($courses, $i) {
+                                return pow($i, count($courses)) - pow($i, $number);
+                            })->toArray(),
+                        ],
+                    ]
+                ])->getBody()->getContents());
+                if (!isset($response->error)) {
+                    $this->vars['results'][] = [
+                        'heading' => 'Exponentielle Gewichte mit Basis ' . $i,
+                        'histPreferences' => $response->histPreferences,
+                        'histCourses' => $response->histCourses,
+                        'mean' => $response->mean,
+                    ];
+                }
+            }
+        }
     }
 }
