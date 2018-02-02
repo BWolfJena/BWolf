c7a4c88e54ffe71e39b8e348cb8f79233b7ec239
Christoph <staudtc@live.de>
2018-02-02 11:43:13 +0100

    Save courses on dragndrop

--- src/plugins/bwolfjena/core/assets/js/course_selection.js
+++ src/plugins/bwolfjena/core/assets/js/course_selection.js
@@ -1,22 +1,27 @@
 var sortableList = document.getElementById('sortableCourseList');
 var itemCount = $('#sortableCourseList').children().length;
 
+function saveOrder() {
+    $.request('onSaveOrder',{
+        data: {
+            order: sortable.toArray(),
+        },
+    });
+}
+
 var sortable =   Sortable.create(sortableList, {
   handle: '.drag-handle',
   onEnd: function(event) {
     var items = $('#sortableCourseList').find('.rank').each(function(index) {
       $(this).text(itemCount-index);
     });
+    saveOrder();
   }
 });
 
 $(document).ready(function(){
   $('#saveOrderButton').on('click touchstart', function(event){
     event.preventDefault();
-    $.request('onSaveOrder',{
-      data: {
-        order: sortable.toArray(),
-      },
-    });
+    saveOrder();
   });
 })
