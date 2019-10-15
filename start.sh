64e4c6bca2b1fc4100d8bb13cd5061e6ffdf8d1f
CSNWEB <christoph@kiron.university>
2019-10-15 22:24:12 +0200

    New simpler docker-compose

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 
 set -e
 
-cd "$(dirname "$0")/laradock"
+cd "$(dirname "$0")/"
 
 sudo docker-compose up -d
 sleep 1
-sudo docker-compose exec --user=laradock workspace composer install
-sudo docker-compose exec --user=laradock workspace php artisan october:up
-sudo docker-compose exec --user=laradock workspace php artisan october:mirror public/
+sudo docker-compose exec web composer install
+sudo docker-compose exec web php artisan october:up
+sudo docker-compose exec web php artisan october:mirror public/
